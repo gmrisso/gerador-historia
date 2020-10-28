3426a849733ca232fbe7791568a6c8dee1302e75
Gilnei Risso <gilnei.risso@gmail.com>
2020-10-27 21:40:34 -0300

    retirar código desnecessário

--- src/pages/Teacher/index.js
+++ src/pages/Teacher/index.js
@@ -17,8 +17,6 @@ export default class Main extends Component{
 		
 		let classroom = Math.floor((Math.random() * 8999)+1000);
 		
-		//classroom = classroom.toUpperCase();
-		
 		this.setState({classroom});
 		
 		socket.emit('create-room',classroom);
@@ -110,78 +108,6 @@ export default class Main extends Component{
 	componentWillUnmount(){
 		
 		socket.close();
-	}
-	
-	addText = () =>{
-		
-		let id = document.getElementsByName("id")[0].value;
-		let title = document.getElementsByName("title")[0].value;
-		let story = document.getElementsByName("story")[0].value;
-		let author = document.getElementsByName("author")[0].value;
-		
-		if(title.trim() === "" || story.trim() === "")
-		return;
-		
-		let keyCards = this.state.place + " | " + this.state.persona + " | " + this.state.action + " | " + this.state.complement;
-		
-		let LStory = this.state.listStories;
-		let classroom = this.state.classroom;
-		
-		LStory.push([classroom,id,keyCards,title,story,author,"Entregue"]);
-		
-		this.setState({listStories:LStory});
-		
-		let containerTexts = document.getElementById("texts");
-				
-		containerTexts.innerHTML = "";
-		
-		let texto;
-		
-		for(story in LStory){
-			
-			let textBox = document.createElement("div");	
-			textBox.setAttribute("class","text-box");
-			textBox.setAttribute("id",LStory[story][1]);			
-			
-			let divKeywords = document.createElement("div");	
-			divKeywords.setAttribute("id","text-keywords");		
-			texto = document.createTextNode(LStory[story][2]);
-			divKeywords.appendChild(texto);
-				
-			let divTitle= document.createElement("div");	
-			divTitle.setAttribute("id","text-title");
-			texto = document.createTextNode(LStory[story][3]);
-			divTitle.appendChild(texto);
-			
-			let divText= document.createElement("div");	
-			divText.setAttribute("id","delivered-text");
-			texto = document.createTextNode(LStory[story][4]);
-			divText.appendChild(texto);
-			
-			let divAuthor= document.createElement("div");	
-			divAuthor.setAttribute("id","text-author");
-			texto = document.createTextNode(LStory[story][5]);
-			divAuthor.appendChild(texto);
-			
-			let divStatus = document.createElement("div");	
-			divStatus.setAttribute("id","status");		
-			texto = document.createTextNode(LStory[story][6]);
-			divStatus.style.color = "green";
-			divStatus.appendChild(texto);
-			
-			textBox.appendChild(divStatus);
-			textBox.appendChild(divKeywords);
-			textBox.appendChild(divTitle);
-			textBox.appendChild(divText);
-			textBox.appendChild(divAuthor);
-			
-			containerTexts.appendChild(textBox);
-			
-		}
-			
-	document.getElementsByName("title")[0].value = "";
-	document.getElementsByName("story")[0].value = "";
-	
 	}
 	
 	render(){
